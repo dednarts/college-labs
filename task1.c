3cb7365405b1e0711fe8b14c0c2cc3373318a872
Nicholas <49667612+dednarts@users.noreply.github.com>
2019-04-17 20:52:03 -0400

    Add files via upload

--- task1.c
+++ task1.c
@@ -1,90 +1,100 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-#define MAX 10
-#define EMPTY -1
 
 typedef struct node_t{
     int value;
     struct node_t *next;
 }node_t;
 
-typedef struct stack_t {
-    node_t *s_top; //points to the first node
-}stack_t; //holds pointer to top of stack
-
-
-void push(stack_t **sPtr,int value); //task1 functions are done
-int pop(stack_t **sPtr);
-void enqueue(node_t **head,node_t **tail,int value);
-int dequeue(node_t **head);
-int is_empty(void);
+void add_node(node_t **head,node_t **tail, int data);
+void print_node(node_t **node);
+void delete(int data, node_t **node);
+void swap(node_t **first, node_t **second);
+void sort(node_t **node);
 
 int main(void){
-    struct node_t *head = NULL;
-    struct node_t *tail = NULL; //global variables until i figure out if this is bad format or nah
-    struct stack_t *sPtr;
-    int temp[MAX];
-    printf("Before Reversing: ");//task2 begins here
-    for(int i=0;i<MAX;i++){
-    enqueue(&head,&tail,i+1);
+    int data;
+    node_t *head = NULL;
+    node_t *tail = NULL;
+    for(int i = 0;i<7;i++){
+        scanf("%d", &data);
+        add_node(&head,&tail,data);
     }
-    printf("\n");
-    for(int i=0;i<MAX;i++){
-    push(&sPtr,dequeue(&head));
-    } 
-    printf("After Reversing: ");
-    for(int i=0;i<MAX;i++){
-       printf("%d ", pop(&sPtr));
-    } 
+    printf("the linked list: ");
+    print_node(&head);
+    swap(&head, &head->next);
+    printf("\nswapping head and second node: ");
+    print_node(&head);
+    sort(&head);
+    printf("\nthe sorted list: ");
+    print_node(&head);
+    printf("\nenter a node to delete: ");
+    scanf("%d", &data);
+    delete(data,&head);
+    printf("the linked list: ");
+    print_node(&head);
+    
     return 0;
 }
 
-void enqueue(node_t **head,node_t **tail,int value){
-    node_t *new_node = (node_t *) malloc(sizeof(node_t) * MAX);
-    new_node->value = value;
+void delete(int data,node_t **node){
+    node_t *temp_node = (node_t *) malloc(sizeof(node_t));
+    temp_node = (*node);
+    while(temp_node != NULL){
+        if(temp_node->value == data){
+            temp_node->value = temp_node->next->value;
+            temp_node->next =  temp_node->next->next;
+            temp_node = (*node);
+            break;
+        }
+        temp_node = temp_node->next;
+    }
+}
+
+void sort(node_t **node){
+    node_t *temp_node = (node_t *) malloc(sizeof(node_t));
+    node_t *sort_node = (node_t *) malloc(sizeof(node_t));
+    temp_node = (*node);
+    sort_node = (*node);
+    while(temp_node != NULL){
+        while(sort_node !=NULL){
+            if(temp_node->value < sort_node->value){
+                swap(&temp_node,&sort_node);
+            }
+            sort_node = sort_node->next;
+        }
+        sort_node =(*node);
+        temp_node = temp_node->next;
+    }
+}
+
+void swap(node_t **first,node_t **second){
+    node_t *temp_node = (node_t *) malloc(sizeof(node_t));
+    temp_node->value = (*first)->value;
+    (*first)->value = (*second)->value;
+    (*second)->value = temp_node->value;
+}
+
+void print_node(node_t **node){
+    node_t *temp_node = (node_t *) malloc(sizeof(node_t));
+    temp_node = (*node);
+    while(temp_node != NULL){
+        printf("%d ",temp_node->value);
+        temp_node = temp_node->next;
+    }
+}
+
+void add_node(node_t **head,node_t **tail, int data){
+    node_t *new_node = (node_t *) malloc(sizeof(node_t));
+    new_node->value = data;
     new_node->next = NULL;
-    printf("%d ",new_node->value);
 
     if((*head) == NULL){
-        (*head) = new_node; //sets the new node equal to the head node initially creating the linked list
+        (*head) = new_node;
     } else {
-        (*tail)->next = new_node; // if the head node is present then it adds on becoming the tail
+        (*tail)->next = new_node;
     }
     (*tail) = new_node;
-    return;
-}
 
-int dequeue(node_t **head){ //remember that queues are first in first order
-    node_t *temp_node;
-    int temp_value;
-
-    if((*head) != NULL){ //checking if the node ISNT empty
-        temp_node = (*head);
-        temp_value = temp_node->value;
-        (*head) = (*head)->next; //iterates
-        free(temp_node);
-    }
-    return temp_value;
-}
-
-void push(stack_t **sPtr,int value){ //adds item to the stack
-    struct node_t *new_node;
-    new_node = (node_t *) malloc(sizeof(node_t));
-    new_node->value = value;
-
-    new_node->next = (*sPtr)->s_top;
-    (*sPtr)->s_top = new_node;
-    return;
-}
-
-int pop(stack_t **sPtr){ //returns the value of the item from the top of the stack
-    struct node_t *temp_ptr;
-    int temp_value;
-
-    temp_ptr = (*sPtr)->s_top; //accesses top node through the memory?
-    temp_value = temp_ptr->value;
-    (*sPtr)->s_top = temp_ptr->next;
-    free(temp_ptr);
-    return temp_value;
 }
